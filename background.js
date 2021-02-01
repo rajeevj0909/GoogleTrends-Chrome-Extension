9bf196823915d742be88fb7e3a58c3b4f421dd8d
Rajeev J <rajeev.jhaj+github@gmail.com>
2021-02-01 19:56:01 +0000

    Updated available options

--- background.js
+++ background.js
@@ -1,3 +1,7 @@
+/*
+Property of RAJEEV J - rajeevj.co.uk
+*/
+
 chrome.runtime.onInstalled.addListener(function() {
   chrome.storage.sync.set({"country_id": 1});
 });
---
+++ trends.js
@@ -0,0 +1,6 @@
+/*chrome.storage.sync.get({"country_id": country});
+document.getElementById("title").innerHTML = country;
+alert(country);
+console.log(country_id);
+console.log(country)*/
+console.log("Ran Page")
